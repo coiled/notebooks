25f98697d8e0e5ae328e3862800fa50ba72a6a79
James Bourbeau <jrbourbeau@gmail.com>
2020-10-08 23:09:34 -0500

    Bump coiled to 0.0.26

--- hyperband/create-notebook.py
+++ hyperband/create-notebook.py
@@ -4,7 +4,7 @@ conda = {
     "channels": ["conda-forge", "pytorch", "defaults"],
     "dependencies": [
         "dask>=2.29.0",
-        "coiled=0.0.25",
+        "coiled=0.0.26",
         "numpy",
         "pandas>=1.1.0",
         "dask-ml",
