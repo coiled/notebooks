65279d3741d0b29c7b94556c61ea96286cc8ad38
James Bourbeau <jrbourbeau@users.noreply.github.com>
2020-11-24 20:07:25 -0600

    Bump coiled to 0.0.29 (#10)

--- hyperband/create-notebook.py
+++ hyperband/create-notebook.py
@@ -4,7 +4,7 @@ conda = {
     "channels": ["conda-forge", "pytorch", "defaults"],
     "dependencies": [
         "dask>=2.29.0",
-        "coiled=0.0.28",
+        "coiled=0.0.29",
         "numpy",
         "pandas>=1.1.0",
         "dask-ml",
--- optuna-xgboost/create-notebook.py
+++ optuna-xgboost/create-notebook.py
@@ -4,7 +4,7 @@ conda = {
     "channels": ["conda-forge"],
     "dependencies": [
         "dask",
-        "coiled=0.0.28",
+        "coiled=0.0.29",
         "optuna",
         "numpy",
         "scikit-learn",
